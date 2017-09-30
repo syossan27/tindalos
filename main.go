c0f09e3d05f3b389dd7659fbc7cd8158ab4f7aad
syossan27 <wisdom1027@gmail.com>
2017-10-01 00:39:13 +0900

    Re-implemented for vim

--- main.go
+++ main.go
@@ -23,45 +23,71 @@ func main() {
 	}
 	defer watcher.Close()
 
-	done := make(chan bool)
-	go func() {
-		for {
-			select {
-			case event := <-watcher.Events:
-				if event.Op.String() == "WRITE" {
-					fileName := event.Name
-					err := exec.Command("goimports", "-w "+fileName).Run()
-					if err != nil {
-						log.Println("Error: ", err)
-					}
-					fmt.Println("Success!!")
-				}
-			case err := <-watcher.Errors:
-				log.Println("Error: ", err)
-			}
-		}
-	}()
-
 	var conf Config
 	_, err = toml.DecodeFile("tindalos.toml", &conf)
 	if err != nil {
 		panic(err)
 	}
 
-	for _, confTarget := range conf.Target {
+	var targets []string
+	for _, target := range conf.Target {
 		baseDir, err := os.Getwd()
 		if err != nil {
 			panic(err)
 		}
-		targets, err := filepath.Glob(baseDir + confTarget)
-		if err != nil {
-			panic(err)
-		}
-		for _, target := range targets {
-			err = watcher.Add(target)
+
+		ts, err := filepath.Glob(baseDir + target)
+		for _, t := range ts {
+			targets = append(targets, t)
 			if err != nil {
 				panic(err)
 			}
+		}
+	}
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				if event.Op&fsnotify.Chmod == fsnotify.Chmod {
+					continue
+				}
+
+				fileName := event.Name
+				for _, t := range targets {
+					if fileName == t {
+						err := exec.Command("goimports", "-w", fileName).Run()
+						if err != nil {
+							log.Println("Error: ", err)
+						}
+					} else {
+						swpFileName := filepath.Dir(t) + "/." + filepath.Base(t) + ".swp"
+						if swpFileName == fileName {
+							fmt.Println(t)
+							err := exec.Command("goimports", "-w", t).Run()
+							if err != nil {
+								log.Println("Error: ", err)
+							}
+						}
+					}
+				}
+				fmt.Println("Success!!")
+			case err := <-watcher.Errors:
+				log.Println("Error: ", err)
+			}
+		}
+	}()
+
+	for _, target := range conf.Target {
+		baseDir, err := os.Getwd()
+		if err != nil {
+			panic(err)
+		}
+
+		err = watcher.Add(filepath.Dir(baseDir + target))
+		if err != nil {
+			panic(err)
 		}
 	}
 
